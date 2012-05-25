d9e22ea7fdb2ef684be15df8f6c3a7aec7e51069
Akira Takahashi <faithandbrave@gmail.com>
2012-05-25 10:52:30 +0900

    fix license

--- boost/range/access.hpp
+++ boost/range/access.hpp
@@ -1,6 +1,15 @@
 #ifndef BOOST_RANGE_ACCESS_INCLUDE
 #define BOOST_RANGE_ACCESS_INCLUDE
 
+// Boost.Range 2.0 Extension library
+// via PStade Oven Library
+//
+// Copyright Akira Takahashi 2011.
+// Copyright Shunsuke Sogame 2005-2007.
+// Use, modification and distribution is subject to the Boost Software License,
+// Version 1.0. (See accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
+
 #include <boost/range/access/at.hpp>
 #include <boost/range/access/back.hpp>
 #include <boost/range/access/front.hpp>
--- boost/range/adaptor/elements.hpp
+++ boost/range/adaptor/elements.hpp
@@ -1,6 +1,15 @@
 #ifndef BOOST_RANGE_ADAPTOR_ELEMENTS_INCLUDE
 #define BOOST_RANGE_ADAPTOR_ELEMENTS_INCLUDE
 
+// Boost.Range 2.0 Extension library
+// via PStade Oven Library
+//
+// Copyright Akira Takahashi 2011.
+// Copyright Shunsuke Sogame 2005-2007.
+// Use, modification and distribution is subject to the Boost Software License,
+// Version 1.0. (See accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
+
 #include <boost/config.hpp>
 #include <boost/fusion/include/at_c.hpp>
 #include <boost/fusion/include/at_key.hpp>
